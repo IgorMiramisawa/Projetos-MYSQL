55eaea373b3ea96a4061406ece2252f0a6da0e75
Igor <Igor.miramisawa@outlook.com>
2022-01-18 00:14:23 -0300

    Spring Boot

--- Projetos Spring/mentalidadesobjetivos/src/main/java/br/com/generation/atividadementalidade/controller/ClasseController.java
+++ Projetos Spring/mentalidadesobjetivos/src/main/java/br/com/generation/atividadementalidade/controller/ClasseController.java
@@ -1,16 +1,18 @@
 package br.com.generation.atividadementalidade.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
-@RequestMapping
+@RestController 
+@RequestMapping ("/mentalidade")
 
 public class ClasseController {
 	
 	@GetMapping
 	public String mentalidade () {
-		return "H"
+		return "Habilidade atenção aos detalhes e mentalidade de persistência" ;
 	}
 
 	
